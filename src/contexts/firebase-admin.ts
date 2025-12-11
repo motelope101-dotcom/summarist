81b72b1dbe3feff2ef3ce1c454e0fdbd2886ea95
motelope101-dotcom <motelope101@gmail.com>
2025-12-10 23:14:20 -0500

    ***** FIRESTORE-SETUP ***** [1]

--- src/contexts/firebase-admin.ts
+++ src/contexts/firebase-admin.ts
@@ -1,4 +1,4 @@
-// src/contexts/firebaseAdmin.ts
+// src/contexts/firebase-admin.ts
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
@@ -6,7 +6,9 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n").trim()
+        : undefined,
     }),
   });
 }
